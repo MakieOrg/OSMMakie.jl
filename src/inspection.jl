a52e2118b83b80ee605a585a09e365e7f7cffe92
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2021-12-20 19:16:48 +0100

    Node inspection up and running

--- src/inspection.jl
+++ src/inspection.jl
@@ -3,16 +3,18 @@
 ##########################################################################################
 
 function Makie.show_data(inspector::DataInspector,
-    plot::GraphPlot{<:Tuple{<:AbstractGraph}}, idx, ::Scatter)
+    plot::OSMPlot{<:Tuple{<:OSMGraph}}, idx, source::Scatter)
+    osm = plot.osm[]
     a = inspector.plot.attributes
     scene = Makie.parent_scene(plot)
 
-    proj_pos = Makie.shift_project(scene, plot, to_ndim(Point3f0, plot[1][][idx], 0))
+    pos = source[1][][idx]
+    proj_pos = Makie.shift_project(scene, plot, to_ndim(Point3f, pos, 0))
     Makie.update_tooltip_alignment!(inspector, proj_pos)
-    ms = plot.markersize[]
 
-    cursor_pos = collect(plot[1][][idx].data[1], plot[1][][idx].data[2])
-    a._display_text[] = node2string(plot.parent.osm[], Float64.(cursor_pos))
+    nn = nearest_node(osm, collect(pos))[1][1][1]
+    a._display_text[] = node2string(osm.nodes[nn])
+    ms = source.markersize[][osm.node_to_index[nn]]
     a._bbox2D[] = Rect2f(proj_pos .- 0.5 .* ms .- Vec2f(5), Vec2f(ms) .+ Vec2f(10))
     a._px_bbox_visible[] = true
     a._bbox_visible[] = false
@@ -21,19 +23,12 @@ function Makie.show_data(inspector::DataInspector,
     return true
 end
 
-function node2string(osm::LightOSM.OSMGraph, cursor_pos::Vector{Float64})
-    nn = nearest_node(osm, cursor_pos)[1][1][1]
-    return node2string(osm.nodes[nn])
-end
-
 function node2string(node::N) where {N<:LightOSM.Node}
-    nodestring = "▶ $(nameof(N))\n"
+    nodestring = "▶ $(nameof(N)) $(node.id)\n"
 
-    nodestring *= "id: $(node.id)\n"
+    nodestring *= "$(node.location)"
 
-    nodestring *= "GeoLocation: $(node.nodes)\n"
-
-    for (key, val) in Pairs(node.tags)
+    for (key, val) in pairs(node.tags)
         nodestring *= "$(key): $(val)\n"
     end
 
