7ebf4c9c756c19e15ed7ab026254e1687ef094a2
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2022-06-08 16:21:28 +0200

    Update basic test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,37 +4,53 @@ using OSMMakie.LightOSM
 using GLMakie
 using StatsBase
 
-##
+## Get data
+
+# download OpenStreetMap data and load as OSMGraph
+osm = graph_from_download(:bbox; # rectangular area
+    minlat = 51.5015, # bottom left corner
+    minlon = -0.0921,
+    maxlat = 51.5154, # top right corner
+    maxlon = -0.0662,
+    network_type = :drive, # download motorways
+    graph_type = :light, # SimpleDiGraph
+    weight_type = :distance
+)
+
+# download OpenStreetMap buildings and load as buildings dict
+buildings = buildings_from_download(:bbox;
+    minlat = 51.5015, # bottom left corner
+    minlon = -0.0921,
+    maxlat = 51.5154, # top right corner
+    maxlon = -0.0662,
+    metadata = true,
+    download_format = :osm,
+);
+
+## Tests
 
 @testset "Basic" begin
-    # download OpenStreetMap data and load as OSMGraph
-    osm = graph_from_download(:bbox; # rectangular area
-        minlat = 51.5015, # bottom left corner
-        minlon = -0.0921,
-        maxlat = 51.5154, # top right corner
-        maxlon = -0.0662,
-        network_type = :drive, # download motorways
-        graph_type = :light, # SimpleDiGraph
-        weight_type = :distance
-    )
-    fig, ax, p = osmplot(osm)
-    DataInspector(fig)
+    @test begin # OSMPlot recipe
+        osmplot(osm)
+        true
+    end
 
-    # download OpenStreetMap buildings and load as buildings dict
-    buildings = buildings_from_download(:bbox;
-        minlat = 51.5015, # bottom left corner
-        minlon = -0.0921,
-        maxlat = 51.5154, # top right corner
-        maxlon = -0.0662,
-        metadata = true,
-        download_format = :osm,
-    );
+    @test begin # DataInspector
+        fig, ax, p = osmplot(osm)
+        DataInspector(fig)
+        true
+    end
 
-    # plot London with buildings
-    fig = Figure()
-    ax = fig[1,1] = Axis(fig; 
-        limits = ((-0.0921, -0.0662), (51.5015, 51.5154)),
-        autolimitaspect = 1 + abs(mean([51.5015, 51.5154])) / 90
-    )
-    p = osmplot!(osm; buildings)
+    # wrong recipe usage
+    @test_throws MethodError osmplot(; osm)
+
+    @test begin # recipe with buildings and limits/autolimitaspect keywords
+        fig = Figure()
+        ax = fig[1,1] = Axis(fig; 
+            limits = ((-0.0921, -0.0662), (51.5015, 51.5154)),
+            autolimitaspect = 1 + abs(mean([51.5015, 51.5154])) / 90
+        )
+        p = osmplot!(osm; buildings)
+        true
+    end
 end
